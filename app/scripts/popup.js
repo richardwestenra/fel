d506f55e50c312fe7d28e576fae38e5f7e279ac6
Richard Westenra <rjwestenra@gmail.com>
2015-06-16 11:58:48 +0100

    Add Refresh rate range slider

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,7 +1,17 @@
+/* globals $ */
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
 
 $('.radio').on('click change', function(){
-	console.log( $(this).attr('name'), $(this).val() );
+	var name = $(this).attr('name');
+	var val = $(this).val();
+	console.log( name, val );
+});
+
+$('.range').on('input change', function(){
+	var name = $(this).attr('name');
+	var val = $(this).val();
+	$('#refreshRateVal').text(val);
+	console.log( name, val );
 });
