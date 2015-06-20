bfeefee8c00376e1499ace3478a5ffb3b035eeff
Richard Westenra <rjwestenra@gmail.com>
2015-06-20 11:10:39 +0100

    Use localStorage to store settings, and update options when popup inputs are clicked

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,24 +2,8 @@
 
 (function(){
 
-	var options = {
-		hostname: ['www.eventbrite.com','www.eventbrite.co.uk'],
-		tickets: 2,
-		autobuy: false,
-		refresh: false,
-		refreshRate: 10
-	};
-	// options.autobuy = true;
-	// options.refresh = true;
-
-
-	// Don't refresh on different hostnames
-	function isCorrectHost(){
-		return options.hostname.indexOf(window.location.hostname) > -1;
-	}
-	if (!isCorrectHost()) {
-		return;
-	}
+	var options = {};
+	var storage = chrome.storage.local;
 
 	var $select = $('#TicketReg').find('select');
 	var $button = $('#primary_cta').find('a');
@@ -27,23 +11,50 @@
 		$button = $('#OrderReg').find('a');
 	}
 
-	if ($select.length) {
-		console.log('Ordering ' + options.tickets + ' tickets');
 
-		$select.val(options.tickets);
+	function isCorrectHost(){
+		return options.hostname.indexOf(window.location.hostname) > -1;
+	}
 
-		if (options.autobuy) {
-			$button[0].click();
-		}
-
-	} else if (options.refresh) {
-		console.log('Refreshing in 1 second...');
-
+	function refresh(){
+		console.log('Refreshing in '+options.refreshRate+'ms...');
 		window.setTimeout(function(){
 			console.log('Refreshing!');
 			window.location.reload();
 		}, options.refreshRate);
-
 	}
+
+
+	function init(){
+		if (!isCorrectHost()) {
+			return;
+		}
+
+		if ($select.length) {
+			console.log('Ordering ' + options.tickets + ' tickets');
+
+			$select.val(options.tickets);
+
+			if (options.autobuy) {
+				$button[0].click();
+			}
+
+		} else if (options.refresh) {
+			refresh();
+		}
+	}
+
+	function loadChanges() {
+		storage.get('fel', function(items) {
+			if (items.fel) {
+				options = items.fel;
+				console.log(options);
+			}
+			init();
+		});
+	}
+
+	loadChanges();
+
 
 })();
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,16 +1,72 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+var options = {
+	hostname: ['www.eventbrite.com','www.eventbrite.co.uk'],
+	tickets: 2,
+	autobuy: false,
+	refresh: false,
+	refreshRate: 10
+};
 
-$('.radio').on('click change', function(){
+var storage = chrome.storage.local;
+
+var timeout;
+function message(msg) {
+	$('.message').text(msg).slideDown(200);
+	clearTimeout(timeout);
+	timeout = setTimeout(function() {
+		$('.message').slideUp(600);
+	}, 3000);
+}
+
+// Convert strings to Boolean/number values
+function convertType(val) {
+	if (val === 'true') {
+		return true;
+	} else if (val === 'false') {
+		return false;
+	} else if ( typeof +val === 'number') {
+		return +val;
+	}
+	return val;
+}
+
+function saveChanges(key, value) {
+	options[key] = value;
+	// Save it using the Chrome extension storage API.
+	storage.set({'fel': options}, function() {
+		// Notify that we saved.
+		message('Setting saved: '+key+' = '+value+'. Reload page to see changes.');
+	});
+}
+
+function loadChanges() {
+	storage.get('fel', function(items) {
+		if (items.fel) {
+			options = items.fel;
+			message('Loaded saved settings.');
+		}
+	});
+}
+
+
+
+loadChanges();
+
+
+$('input').on('input click change', function(){
 	var name = $(this).attr('name');
-	var val = $(this).val();
-	console.log( name, val );
+	var val = convertType( $(this).val() );
+	if ($(this).hasClass('range')) {
+		val *= 10; // convert to time in ms
+		var text = val / 1000 + ' seconds'; // convert to secs
+		if (val === 1000 ) {
+			text = '1 second';
+		} else if ( val < 1000) {
+			text = val + ' ms';
+		}
+		$('#refreshRateVal').text(text);
+	}
+	saveChanges(name, val);
 });
 
-$('.range').on('input change', function(){
-	var name = $(this).attr('name');
-	var val = $(this).val();
-	$('#refreshRateVal').text(val);
-	console.log( name, val );
-});
